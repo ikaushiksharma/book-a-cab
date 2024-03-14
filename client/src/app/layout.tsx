638d31a63473e4e43e345a22626f63f1ab3b1e10
Kaushik Sharma <kaushik445566@gmail.com>
2024-03-14 16:40:09 +0530

    client: init

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Cab Booking App",
+  description: "Assigment for Scaler Internship Program 2024",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>{children}</body>
+    </html>
+  );
+}
